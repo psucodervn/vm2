83dbb21fbc418d384f7abe9a017eda65aa546a73
Hưng Lê <hunglm@vzota.com.vn>
2018-07-26 07:09:44 +0700

    Add view log

--- cmd/log.go
+++ cmd/log.go
@@ -1,66 +1,71 @@
 package cmd
 
 import (
-  "github.com/spf13/cobra"
-  "github.com/manifoldco/promptui"
-  "fmt"
-  "github.com/psucodervn/vm2/pkg/pm2"
-  "strings"
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"github.com/psucodervn/vm2/pkg/pm2"
+	"github.com/spf13/cobra"
+	"log"
+	"strings"
 )
 
 // logCmd represents the log command
 var logCmd = &cobra.Command{
-  Use:   "log",
-  Short: "View pm2 log",
-  Run:   viewLog,
+	Use:   "log",
+	Short: "View pm2 log",
+	Run:   viewLog,
 }
 
 func init() {
-  rootCmd.AddCommand(logCmd)
+	rootCmd.AddCommand(logCmd)
 }
 
 type pepper struct {
-  Name     string
-  HeatUnit int
-  Peppers  int
+	Name     string
+	HeatUnit int
+	Peppers  int
 }
 
 func viewLog(cmd *cobra.Command, args []string) {
-  procs, _ := pm2.List()
+	procs, _ := pm2.List()
 
-  templates := &promptui.SelectTemplates{
-    Label:    "{{ .Name }}?",
-    Active:   "> {{ .Name | green }} (id = {{ .PMID | red }})",
-    Inactive: "  {{ .Name | green }} (id = {{ .PMID | red }})",
-    Selected: "> {{ .Name | red | green }}",
-    Details: `--------- Process ----------
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ .Name }}?",
+		Active:   "> {{ .Name | green }} (id = {{ .PMID | red }})",
+		Inactive: "  {{ .Name | green }} (id = {{ .PMID | red }})",
+		Selected: "> pm2 log \"{{ .Name | red | green }}\"",
+		Details: `--------- Process ----------
 {{ "Name:" | faint }}	{{ .Name }}
 {{ "Process id:" | faint }}	{{ .PMID }}
 {{ "Exec path:" | faint }}	{{ .Env.ExecPath }}
 {{ "Status:" | faint }}	{{ .Env.Status }}`,
-  }
+	}
 
-  searcher := func(input string, index int) bool {
-    pepper := procs[index]
-    name := strings.Replace(strings.ToLower(pepper.Name), " ", "", -1)
-    id := strings.Replace(strings.ToLower(fmt.Sprintf("%d", pepper.PMID)), " ", "", -1)
-    input = strings.Replace(strings.ToLower(input), " ", "", -1)
+	searcher := func(input string, index int) bool {
+		proc := procs[index]
+		name := strings.Replace(strings.ToLower(proc.Name), " ", "", -1)
+		id := strings.Replace(strings.ToLower(fmt.Sprintf("%d", proc.PMID)), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
 
-    return strings.Contains(name, input) || strings.Contains(id, input)
-  }
+		return strings.Contains(name, input) || strings.Contains(id, input)
+	}
 
-  prompt := promptui.Select{
-    Label:     "Choose process to view log:",
-    Items:     procs,
-    Templates: templates,
-    Size:      10,
-    Searcher:  searcher,
-  }
-  prompt.StartInSearchMode = true
-  _, _, err := prompt.Run()
+	prompt := promptui.Select{
+		Label:     "Choose process to view log:",
+		Items:     procs,
+		Templates: templates,
+		Size:      10,
+		Searcher:  searcher,
+	}
+	prompt.StartInSearchMode = true
+	idx, _, err := prompt.Run()
 
-  if err != nil {
-    fmt.Printf("Prompt failed %v\n", err)
-    return
-  }
+	if err != nil {
+		fmt.Printf("Prompt failed: %v\n", err)
+		return
+	}
+
+	if err := pm2.ViewLog(procs[idx]); err != nil {
+		log.Fatalln(err)
+	}
 }
--- pkg/pm2/pm2.go
+++ pkg/pm2/pm2.go
@@ -1,53 +1,72 @@
 package pm2
 
 import (
-  "os/exec"
-  "bytes"
-  "github.com/pkg/errors"
-  "encoding/json"
-  "log"
+	"bytes"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
 )
 
 type Monit struct {
-  Memory int `json:"memory"`
-  CPU    int `json:"cpu"`
+	Memory int `json:"memory"`
+	CPU    int `json:"cpu"`
 }
 
 type Env struct {
-  ExitCode int    `json:"exit_code"`
-  Status   string `json:"status"`
-  ExecPath string `json:"pm_exec_path"`
-  ExecMode string `json:"exec_mode"`
-  Watch    bool   `json:"watch"`
+	ExitCode int    `json:"exit_code"`
+	Status   string `json:"status"`
+	ExecPath string `json:"pm_exec_path"`
+	ExecMode string `json:"exec_mode"`
+	Watch    bool   `json:"watch"`
 }
 
 type Process struct {
-  Name string `json:"name"`
-  PID  int    `json:"pid"`
-  PMID int    `json:"pm_id"`
-  Env  Env    `json:"pm2_env"`
+	Name string `json:"name"`
+	PID  int    `json:"pid"`
+	PMID int    `json:"pm_id"`
+	Env  Env    `json:"pm2_env"`
 }
 
 var (
-  ErrPM2PathNotSet = errors.New("pm2 executable path is not set")
+	ErrPM2PathNotSet = errors.New("pm2 executable path is not set")
 )
 
 func List() ([]Process, error) {
-  cmd := exec.Command("pm2", "jlist")
-  var stdout bytes.Buffer
-  var stderr bytes.Buffer
-  cmd.Stdout = &stdout
-  cmd.Stderr = &stderr
+	cmd := exec.Command("pm2", "jlist")
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
-  if err := cmd.Run(); err != nil {
-    log.Println(stderr.String())
-    return nil, err
-  }
+	if err := cmd.Run(); err != nil {
+		log.Println(stderr.String())
+		return nil, err
+	}
 
-  var procs []Process
-  if err := json.Unmarshal(stdout.Bytes(), &procs); err != nil {
-    return nil, err
-  }
+	var procs []Process
+	if err := json.Unmarshal(stdout.Bytes(), &procs); err != nil {
+		return nil, err
+	}
 
-  return procs, nil
+	return procs, nil
+}
+
+func ViewLog(proc Process) error {
+	cmd := exec.Command("pm2", "log", strconv.Itoa(proc.PMID))
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	go io.Copy(os.Stdout, stdout)
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
 }
